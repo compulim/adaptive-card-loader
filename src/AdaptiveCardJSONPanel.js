a506e22ab7a66f7520cef4722092eeb5ad07b14b
William Wong <compulim@hotmail.com>
2021-08-11 18:29:32 -0700

    Cosmetics

--- src/AdaptiveCardJSONPanel.js
+++ src/AdaptiveCardJSONPanel.js
@@ -1,3 +1,4 @@
+import { parse } from 'json5';
 import { useCallback } from 'react';
 import classNames from 'classnames';
 
@@ -11,8 +12,15 @@ const AdaptiveCardJSONPanel = ({ className, onChange, value }) => {
     [onChange]
   );
 
+  const handlePrettify = useCallback(() => {
+    onChange(JSON.stringify(parse(value), null, 2) + '\n');
+  }, [onChange]);
+
   return (
     <div className="ac-json-panel">
+      <div className="ac-json-panel--buttons">
+        <button onClick={handlePrettify}>Make it pretty</button>
+      </div>
       <textarea
         className={classNames('ac-json-panel__text-area', className)}
         onChange={handleChange}
